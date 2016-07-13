bc0c6f9058cd9688c64a7130f1e38a4ade5a27c1
GREAT BIG BUSHY BEARD <simon.whyte.lb@gmail.com>
2016-07-13 17:59:05 +0100

    Fixed an error in the mod template.

--- mods/BeardLib/Examples/Mod template/Core.lua
+++ mods/BeardLib/Examples/Mod template/Core.lua
@@ -8,7 +8,7 @@ YOURMODCore = YOURMODCore or class(ModCore)
 function YOURMODCore:init()
 	--Calling the base function for init from ModCore
 	--self_tbl, config path, auto load modules, auto post init modules
-	self.super.init(self, ModPath .. "config.xml", true, false)
+	self.super.init(self, ModPath .. "config.xml", true, true)
 end
 
 if not _G.YOURMOD then
