327ad5f6521c5a794dd7b19e56ad27a26c782e1a
Luffy <danielandro11@gmail.com>
2016-09-22 01:35:17 +0300

    Added more possible functions for mrotation

--- mods/BeardLib/Classes/utils.lua
+++ mods/BeardLib/Classes/utils.lua
@@ -15,6 +15,25 @@ function table.merge(og_table, new_table)
 	return og_table
 end
 
+function mrotation.copy(rot)
+    if rot then
+        return Rotation(rot:yaw(), rot:pitch(), rot:roll())
+    end
+    return Rotation()
+end
+
+function mrotation.set_yaw(rot, yaw)
+    return mrotation.set_yaw_pitch_roll(rot, yaw, rot:pitch(), rot:roll())
+end
+
+function mrotation.set_pitch(rot, pitch)
+    return mrotation.set_yaw_pitch_roll(rot, rot:yaw(), pitch, rot:roll())
+end
+
+function mrotation.set_roll(rot, roll)
+    return mrotation.set_yaw_pitch_roll(rot, rot:yaw(), rot:pitch(), roll)
+end
+
 function table.add(t, items)
 	for i, sub_item in ipairs(items) do
 		if t[i] then
