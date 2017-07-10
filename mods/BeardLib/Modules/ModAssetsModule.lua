5b3891cf23cd6888bcfb8515a984d799c5137e2e
Luffy <danielandro11@gmail.com>
2017-07-10 20:25:46 +0300

    Added is_standalone value to ModAssetsModule
    the value will prevent maps that can't be just downloaded and require differnet mods(ex: out of frame)

--- mods/BeardLib/Modules/ModAssetsModule.lua
+++ mods/BeardLib/Modules/ModAssetsModule.lua
@@ -40,12 +40,11 @@ function ModAssetsModule:init(core_mod, config)
 
     self.folder_names = self._config.use_local_dir and table.remove(string.split(self._mod.ModPath, "/")) or (type(self._config.folder_name) == "string" and {self._config.folder_name} or BeardLib.Utils:RemoveNonNumberIndexes(self._config.folder_name))
     self.install_directory = self._config.use_local_path and BeardLib.Utils.Path:GetDirectory(self._mod.ModPath) or (self._config.install_directory and self._mod:GetRealFilePath(self._config.install_directory, self) or BeardLib.config.mod_override_dir)
-
     self.version_file = self._config.version_file and self._mod:GetRealFilePath(self._config.version_file, self) or BeardLib.Utils.Path:Combine(self.install_directory, self.folder_names[1], self._default_version_file)
     self._version = 0
-
+    
     self._update_manager_id = self._mod.Name .. self._name
-    self._mod.update_key = self.id
+    self._mod.update_key = (self._config.is_standalone ~= false) and self.id
     self:RetrieveCurrentVersion()
 
     if not self._config.manual_check then
