aa3ce1565681d56b57404430b7390916467e4391
Daniel.R <danielandro11@gmail.com>
2018-03-17 22:28:27 +0200

    Crash fix

--- mods/BeardLib/Modules/MeleeModule.lua
+++ mods/BeardLib/Modules/MeleeModule.lua
@@ -11,10 +11,10 @@ function MeleeModule:init(core_mod, config)
 end
 
 local default_melee = "kabar"
-function MeleeModule:GetBasedOn(w_self, based_on)
-    w_self = w_self or tweak_data.blackmarket.melee_weapons
+function MeleeModule:GetBasedOn(bm_self, based_on)
+    bm_self = bm_self or tweak_data.blackmarket.melee_weapons
     based_on = based_on or self._config.based_on
-    if based_on and w_self[based_on] then
+    if based_on and bm_self[based_on] then
         return based_on
     else
         return default_melee
@@ -30,7 +30,7 @@ function MeleeModule:RegisterHook()
             return
         end
 
-        local data = table.merge(deep_clone(bm_self.melee_weapons[self:GetBasedOn()]), table.merge({
+        local data = table.merge(deep_clone(bm_self.melee_weapons[self:GetBasedOn(self)]), table.merge({
             name_id = "bm_melee_" .. self._config.id,
             dlc = self.defaults.dlc,
             custom = true,
