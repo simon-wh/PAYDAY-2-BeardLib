d1644f29cd49bc49f51ac02768d1020e3fa90282
Luffy <danielandro11@gmail.com>
2018-03-21 20:35:07 +0200

    Corrected function name

--- mods/BeardLib/Classes/Dialogs/FileBrowserDialog.lua
+++ mods/BeardLib/Classes/Dialogs/FileBrowserDialog.lua
@@ -116,8 +116,8 @@ function FileBrowserDialog:Browse(where, params)
     if self._browse_func then  
         f, d = self._browse_func(self)
     else
-        f = FileIO:getFiles(where)
-        d = FileIO:getFolders(where)
+        f = FileIO:GetFiles(where)
+        d = FileIO:GetFolders(where)
     end
     if self._search:len() > 0 then
         local temp_f = clone(f)
