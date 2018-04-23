1674cfa42562bef88e7214ff8ed75323189d0382
Luffy <danielandro11@gmail.com>
2018-04-23 19:11:31 +0300

    Changed false or true to NotNil

--- mods/BeardLib/Modules/MusicModule.lua
+++ mods/BeardLib/Modules/MusicModule.lua
@@ -32,7 +32,7 @@ function MusicModule:RegisterHook()
 				v.alt_source = Path:Combine(dir, v.alt_source)
 				v.alt_start_source = v.alt_start_source and Path:Combine(dir, v.alt_start_source)
 				v.alt_chance = v.alt_chance and tonumber(v.alt_chance) or 0.1
-				v.allow_switch = v.allow_switch ~= nil and v.allow_switch or true
+				v.allow_switch = NotNil(v.allow_switch, true)
 			end
 			if v.source then
 				v.source = dir .. v.source
