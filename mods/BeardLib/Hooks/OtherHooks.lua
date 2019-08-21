5fc71b5a0974be2616008a1e2ed623dd00e65880
Daniel.R <danielandro11@gmail.com>
2019-08-21 04:18:21 +0300

    Added a check for units alongside icon_check

--- mods/BeardLib/Hooks/OtherHooks.lua
+++ mods/BeardLib/Hooks/OtherHooks.lua
@@ -3,29 +3,40 @@ local Hooks = Hooks
 
 if F == "tweakdata" then
 	TweakDataHelper:Apply()
-	local function icon_check(list, folder, friendly_name)
+	local function icon_and_unit_check(list, folder, friendly_name, uses_texture_val, only_check_units)
 		for id, thing in pairs(list) do
-			if thing.custom and not thing.hidden then
-				if folder ~= "mods" or thing.pcs then
-					local guis_catalog = "guis/"
-					local bundle_folder = thing.texture_bundle_folder
-					if bundle_folder then
-						guis_catalog = guis_catalog .. "dlcs/" .. tostring(bundle_folder) .. "/"
+			if thing.custom and not id:ends("_npc") then
+				if not only_check_units and not thing.hidden then
+					if folder ~= "mods" or thing.pcs then
+						local guis_catalog = "guis/"
+						local bundle_folder = thing.texture_bundle_folder
+						if bundle_folder then
+							guis_catalog = guis_catalog .. "dlcs/" .. tostring(bundle_folder) .. "/"
+						end
+
+						guis_catalog = guis_catalog .. "textures/pd2/blackmarket/icons/"..folder.."/"
+						local tex = uses_texture_val and thing.texture or guis_catalog .. id
+						if not DB:has(Idstring("texture"), tex) then
+							local mod = BeardLib.Utils:FindModWithPath(thing.mod_path) or BeardLib
+							mod:Err("Icon for %s %s doesn't exist path: %s", tostring(friendly_name), tostring(id), tostring(tex))
+						end
 					end
-					guis_catalog = guis_catalog .. "textures/pd2/blackmarket/icons/"..folder.."/"
-					if not DB:has(Idstring("texture"), guis_catalog .. id) then
+				end
+				if thing.unit then
+					if not DB:has(Idstring("unit"), thing.unit) then
 						local mod = BeardLib.Utils:FindModWithPath(thing.mod_path) or BeardLib
-						mod:Err("Icon for %s %s doesn't exist path: %s", tostring(friendly_name), tostring(id), tostring(guis_catalog .. id))
+						mod:Err("Unit for %s %s doesn't exist path: %s", tostring(friendly_name), tostring(id), tostring(thing.unit))
 					end
 				end
 			end
 		end
 	end
-	icon_check(tweak_data.weapon, "weapons", "weapon")
-	icon_check(tweak_data.weapon.factory.parts, "mods", "weapon mod")
-	icon_check(tweak_data.blackmarket.melee_weapons, "melee_weapons", "melee weapon")
-	icon_check(tweak_data.blackmarket.textures, "textures", "mask pattern")
-	icon_check(tweak_data.blackmarket.materials, "materials", "mask material") 	
+	icon_and_unit_check(tweak_data.weapon, "weapons", "weapon")
+	icon_and_unit_check(tweak_data.weapon.factory, "weapons", "weapon", false, true)
+	icon_and_unit_check(tweak_data.weapon.factory.parts, "mods", "weapon mod")
+	icon_and_unit_check(tweak_data.blackmarket.melee_weapons, "melee_weapons", "melee weapon")
+	icon_and_unit_check(tweak_data.blackmarket.textures, "textures", "mask pattern", true)
+	icon_and_unit_check(tweak_data.blackmarket.materials, "materials", "mask material")
 elseif F == "tweakdatapd2" then
 	Hooks:PostHook(WeaponFactoryTweakData, "_init_content_unfinished", "CallWeaponFactoryAdditionHooks", function(self)
 		Hooks:Call("BeardLibCreateCustomWeapons", self)
