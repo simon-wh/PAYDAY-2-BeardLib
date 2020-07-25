2a62e0dcb34a0bcf46440c6112da1fc5a2e01910
Luffyyy <danielandro11@gmail.com>
2020-07-25 22:40:47 +0300

    Allow <hooks> or <Hooks> to set a file for the child <hook>s
    Allowing you to make one file for a few small hooks without having to repeat yourself many times

--- Modules/Utils/HooksModule.lua
+++ Modules/Utils/HooksModule.lua
@@ -9,6 +9,7 @@ function HooksModule:Load(config, prev_dir)
     for _, hook in ipairs(config) do
         if hook._meta == "hook" then
             hook.pre = NotNil(hook.pre, config.pre, hook.type == PRE)
+            hook.file = hook.file or config.file
             type = type or hook.type or (hook.pre and PRE) or (hook.post and POST)
             self._mod:RegisterHook(hook.source_file, dir and Path:Combine(dir, hook.file) or hook.file, hook.pre == true)
         elseif hook._meta == "hooks" then
