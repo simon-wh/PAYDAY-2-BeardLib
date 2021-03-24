720843c4509d744b0858ab8bd3ef069650208031
Daniel R <luffydafloffi@gmail.com>
2021-03-24 13:46:35 +0200

    New module: HUDIconModule

---
+++ Modules/Addons/HUDIconModule.lua
@@ -0,0 +1,24 @@
+HUDIconModule = HUDIconModule or BeardLib:ModuleClass("HUDIcon", ModuleBase)
+
+-- Adds an icon to tweak_data.hud_icons
+
+function HUDIconModule:init(...)
+    self.required_params = table.add(clone(self.required_params), {"id", "texture"})
+	return HUDIconModule.super.init(self, ...)
+end
+
+function HUDIconModule:Load()
+    local rect = self._config.rect
+    local rect_tbl = nil
+    if rect then
+        rect = string.split(rect, " ")
+        rect_tbl = {}
+        for _, num in pairs(rect) do
+            table.insert(rect_tbl, tonumber(num) or 0)
+        end
+    end
+    TweakDataHelper:ModifyTweak(table.merge({
+		texture_rect = rect_tbl,
+        custom = true,
+    }, self._config), "hud_icons", self._config.id)
+end
