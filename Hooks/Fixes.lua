305e6d1449f9486e448b3de2bd8177344ad964e5
Daniel R <luffydafloffi@gmail.com>
2021-04-10 15:45:16 +0300

    Fixed a crash

--- Hooks/Fixes.lua
+++ Hooks/Fixes.lua
@@ -686,7 +686,7 @@ elseif F == "platformmanager" then
     Hooks:PostHook(WinPlatformManager, "set_rich_presence", "FixCustomHeistStatus", function(self)
         if not Global.game_settings.single_player and Global.game_settings.permission ~= "private" and name ~= "Idle" and managers.network.matchmake.lobby_handler  then
             local job = managers.job:current_job_data()
-            if job.custom then
+            if job and job.custom then
                 Steam:set_rich_presence("steam_display", "#raw_status")
             end
         end
