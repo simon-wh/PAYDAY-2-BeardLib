e588c8c8c50b90dab3b64ca135c4f18ee82b865f
Daniel R <luffydafloffi@gmail.com>
2021-07-02 23:59:47 +0300

    Let highlight image be inherited in menu ui

--- Classes/UI/MenuUI/Item.lua
+++ Classes/UI/MenuUI/Item.lua
@@ -275,6 +275,7 @@ function Item:WorkParams(params)
 	self:WorkParam("bg_callbacks")
 	self:WorkParam("no_slide")
 	self:WorkParam("round_sliding", 0)
+	self:WorkParam("highlight_image")
 
     if not self.MENU then
         self:WorkParam("align_method", "grid_from_right")
