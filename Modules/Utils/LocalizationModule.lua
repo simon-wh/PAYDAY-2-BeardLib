948100cd6e94b2ecd584eb6ee6e3fbc53eb71a38
Luffy <luffydafloffi@gmail.com>
2021-09-09 19:49:03 +0300

    Attempt to first load default language (english usually) localization
    Instead of the old default which was a single file

--- Modules/Utils/LocalizationModule.lua
+++ Modules/Utils/LocalizationModule.lua
@@ -6,6 +6,10 @@ function LocalizationModule:Load()
     self.Localizations = {}
 
     self.DefaultLocalization = self._config.default
+    --Since groups are a thing now we shouldn't fallback to a single file
+    --Therefore I recommend now against using default value and instead (if necessary) use default_language
+    --Usually it's better to keep it english.
+    self.DefaultLanguage = self._config.default_language or "english"
     self:LoadGroup(self._config, self.LocalizationDirectory)
 
     if managers.localization then
@@ -39,7 +43,6 @@ function LocalizationModule:LoadGroup(data, directory, language)
 end
 
 function LocalizationModule:LoadLocalization()
-    local path
     local lang_key = (LocalizationModule.forced_language or SystemInfo:language()):key()
     local latam = string.key("latam")
     -- Fallback to Spanish if there is no specific localization for Latin American Spanish
@@ -47,14 +50,16 @@ function LocalizationModule:LoadLocalization()
         lang_key = string.key("spanish")
     end
 
-    if self.Localizations[lang_key] then
-        for _, path in pairs(self.Localizations[lang_key]) do
+    local loc = self.Localizations[lang_key] or self.Localizations[self.DefaultLanguage]
+    if loc then
+        for _, path in pairs(loc) do
             if not FileIO:LoadLocalization(path) then
                 self:Err("Language file has errors and cannot be loaded! Path %s", path)
             end
         end
-    else
-        if not FileIO:LoadLocalization(Path:Combine(self.LocalizationDirectory, self.DefaultLocalization)) then
+    else -- Legacy
+        local path = Path:Combine(self.LocalizationDirectory, self.DefaultLocalization)
+        if not FileIO:LoadLocalization(path) then
             self:Err("Language file has errors and cannot be loaded! Path %s", path)
         end
     end
