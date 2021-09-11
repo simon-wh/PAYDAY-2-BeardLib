53cd0e796bd8b8dfb012e72c0e42a1ceab96b0ff
Connie Price <contact@connorprice.co.uk>
2021-09-11 15:58:45 +0100

    Fixed a bug with glove variant DLC unlocking if your save is relatively new. Found an alternative solution which let me remove some function overriding.

--- Hooks/PlayerStyleGloveHooks.lua
+++ Hooks/PlayerStyleGloveHooks.lua
@@ -1225,125 +1225,45 @@ elseif F == "blackmarketgui" then
 		self:_preview_gloves_and_variation(glove_id, glove_variation, callback(self, self, "reload"))
 	end
 elseif F == "dlcmanager" then
-	Hooks:PostHook(GenericDLCManager, "give_dlc_package", "BeardLibGiveDLCGlovesVariations", function(self)
+	-- Just do our unlocking first, and then kill them off so the vanilla DLC check doesn't try and unlock them.
+
+	Hooks:PreHook(GenericDLCManager, "give_dlc_package", "BeardLibGiveDLCGlovesVariations", function(self)
 		for package_id, data in pairs(tweak_data.dlc) do
-			if self:is_dlc_unlocked(package_id) then
-				for _, loot_drop in ipairs(data.content.loot_drops or {}) do
+			if self:is_dlc_unlocked(package_id) and not Global.dlc_save.packages[package_id] then
+				local loot_drops = data.content.loot_drops or {}
+
+				for index, loot_drop in ipairs(loot_drops) do
 					local loot_drop = #loot_drop > 0 and loot_drop[math.random(#loot_drop)] or loot_drop
 
 					if loot_drop.type_items == "glove_variations" then
 						managers.blackmarket:on_aquired_glove_variation(loot_drop.item_entry[1], loot_drop.item_entry[2])
+
+						loot_drops[index] = nil
 					end
 				end
 			end
 		end
 	end)
 
-	-- Kind of annoying that I have to override this whole thing to stop the game exploding.
-	function GenericDLCManager:give_missing_package()
-		local name_converter = {
-			colors = "color",
-			materials = "material",
-			textures = "pattern"
-		}
-		local entry, global_value, passed, has_item, name, check_loot_drop = nil
-
+	Hooks:PreHook(GenericDLCManager, "give_missing_package", "BeardLibGiveMissingDLCGlovesVariations", function(self)
 		for package_id, data in pairs(tweak_data.dlc) do
 			if Global.dlc_save.packages[package_id] and self:is_dlc_unlocked(package_id) then
-				for _, loot_drop in ipairs(data.content and data.content.loot_drops or {}) do
+				local loot_drops = data.content and data.content.loot_drops or {}
+
+				for index, loot_drop in ipairs(loot_drops) do
 					check_loot_drop = #loot_drop == 0
-
-					if check_loot_drop and loot_drop.type_items == "armor_skins" then
-						entry = tweak_data.economy.armor_skins[loot_drop.item_entry]
-						has_item = managers.blackmarket:armor_skin_unlocked(loot_drop.item_entry)
-
-						if not entry.steam_economy and not has_item then
-							managers.blackmarket:on_aquired_armor_skin(loot_drop.item_entry)
-						end
-
-						check_loot_drop = false
-					end
-
-					if check_loot_drop and loot_drop.type_items == "player_styles" then
-						if not managers.blackmarket:player_style_unlocked(loot_drop.item_entry) then
-							managers.blackmarket:on_aquired_player_style(loot_drop.item_entry)
-						end
-
-						check_loot_drop = false
-					end
-
-					if check_loot_drop and loot_drop.type_items == "suit_variations" then
-						if not managers.blackmarket:suit_variation_unlocked(loot_drop.item_entry[1], loot_drop.item_entry[2]) then
-							managers.blackmarket:on_aquired_suit_variation(loot_drop.item_entry[1], loot_drop.item_entry[2])
-						end
-
-						check_loot_drop = false
-					end
-
-					if check_loot_drop and loot_drop.type_items == "gloves" then
-						if not managers.blackmarket:glove_id_unlocked(loot_drop.item_entry) then
-							managers.blackmarket:on_aquired_glove_id(loot_drop.item_entry)
-						end
-
-						check_loot_drop = false
-					end
 
 					if check_loot_drop and loot_drop.type_items == "glove_variations" then
 						if not managers.blackmarket:glove_variation_unlocked(loot_drop.item_entry[1], loot_drop.item_entry[2]) then
 							managers.blackmarket:on_aquired_glove_variation(loot_drop.item_entry[1], loot_drop.item_entry[2])
 						end
 
-						check_loot_drop = false
-					end
-
-					if check_loot_drop then
-						entry = tweak_data.blackmarket[loot_drop.type_items][loot_drop.item_entry]
-						global_value = loot_drop.global_value or data.content.loot_global_value or package_id
-						passed = false
-
-						if (loot_drop.type_items == "weapon_mods" or loot_drop.type_items == "weapon_skins") and entry.is_a_unlockable then
-							has_item = managers.blackmarket:get_item_amount(global_value, loot_drop.type_items, loot_drop.item_entry, true) > 0
-							passed = not has_item
-						elseif loot_drop.type_items ~= "weapon_mods" and entry.value == 0 then
-							has_item = managers.blackmarket:get_item_amount(global_value, loot_drop.type_items, loot_drop.item_entry, true) > 0
-
-							if not has_item then
-								if loot_drop.type_items == "masks" then
-									for slot, crafted in pairs(Global.blackmarket_manager.crafted_items.masks) do
-										if slot ~= 1 and crafted.mask_id == loot_drop.item_entry and crafted.global_value == global_value then
-											has_item = true
-
-											break
-										end
-									end
-								elseif loot_drop.type_items == "materials" or loot_drop.type_items == "textures" or loot_drop.type_items == "colors" then
-									for slot, crafted in pairs(Global.blackmarket_manager.crafted_items.masks) do
-										if slot ~= 1 then
-											name = name_converter[loot_drop.type_items]
-
-											if crafted.blueprint[name].id == loot_drop.item_entry and crafted.blueprint[name].global_value == global_value then
-												has_item = true
-
-												break
-											end
-										end
-									end
-								end
-
-								passed = not has_item
-							end
-						end
-
-						if passed then
-							for i = 1, loot_drop.amount or 1 do
-								managers.blackmarket:add_to_inventory(global_value, loot_drop.type_items, loot_drop.item_entry)
-							end
-						end
+						loot_drops[index] = nil
 					end
 				end
 			end
 		end
-	end
+	end)
 elseif F == "infamymanagernew" then
 	function InfamyManager:reward_glove_variations(global_value, category, glove_id, glove_variation)
 		managers.blackmarket:on_aquired_glove_variation(glove_id, glove_variation)
