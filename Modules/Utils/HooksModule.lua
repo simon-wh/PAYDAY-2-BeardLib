c0144ccded8ee9a229d011ab3619a94933df0854
Luffyyy <luffydafloffi@gmail.com>
2023-08-12 06:20:40 +0300

    Support BLT-style hooks

--- Modules/Utils/HooksModule.lua
+++ Modules/Utils/HooksModule.lua
@@ -7,12 +7,12 @@ function HooksModule:Load(config, prev_dir)
     local dir = self:GetPath(config.directory, prev_dir)
     config.pre = NotNil(config.pre, config.type == PRE)
     for _, hook in ipairs(config) do
-        if hook._meta == "hook" then
-            hook.pre = NotNil(hook.pre, config.pre, hook.type == PRE)
-            hook.file = hook.file or config.file
-            type = type or hook.type or (hook.pre and PRE) or (hook.post and POST)
-            self._mod:RegisterHook(hook.source_file, dir and Path:Combine(dir, hook.file) or hook.file, hook.pre == true)
-        elseif hook._meta == "hooks" then
+        if hook._meta == "hook" or hook._meta == POST or hook._meta == PRE then
+            local file = hook.file or config.file or hook.script_path or config.script_path
+            local pre = hook.type == PRE or config.type == PRE or hook.pre or hook._meta == PRE
+
+            self._mod:RegisterHook(hook.source_file or hook.hook_id, dir and Path:Combine(dir, file) or file, pre)
+        elseif hook._meta == "hooks" or hook._meta == "group" then
             self:Load(hook, dir)
         end
     end
